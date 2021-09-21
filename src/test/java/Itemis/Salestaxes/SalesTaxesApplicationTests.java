bd0b02a777587fa29ec841bbe0738da47321d385
sahob001 <74726168+sahob001@users.noreply.github.com>
2021-09-21 16:28:56 +0200

    Spring Boot create Projekt

---
+++ src/test/java/Itemis/Salestaxes/SalesTaxesApplicationTests.java
@@ -0,0 +1,13 @@
+package Itemis.Salestaxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesTaxesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
